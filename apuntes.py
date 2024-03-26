438a959754b2134f11bb6da095f8056f58e73d55
javihb <javier.holgado.barreno@gmail.com>
2024-03-26 12:26:41 +0100

    mañana 26/03/24

---
+++ apuntes.py
@@ -0,0 +1,32 @@
+#Variables
+var = 4
+
+#Se pueden generar variables en cadena: 
+nombre, apellidos = "Javier" , "holgado" 
+
+#Funciones fáciles:
+#Cuenta cadenas (solo string)
+len("Esto es una cadena")
+
+#Tipear variables (no tiene ninguna restriccion):
+dirr: str = "La ladera"
+
+# Operaciones matemáticas.
+# + - * / %....
+#Division aproximada "floor division"
+10 // 3
+#Exponentes 
+2 ** 3
+
+
+# "+" Tambien sirve para concatenar.
+print("Buenos" + "dias")
+# El '*' tambien multiplica el string.
+print("Hola" * 5)
+
+#Operaciones comparativos.
+print(3 <= 4)  
+3 != 3
+
+# Comparadores logicos
+# AND OR y NOT 
--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,13 @@
 print("Hola Mundo")
 #Variables
 var = 4
+
+#Se pueden generar variables en cadena: 
+nombre, apellidos = "Javier" , "holgado" 
+
+#Funciones fáciles:
+#Cuenta cadenas (solo string)
+len("Esto es una cadena")
+
+#Tipear variables (no tiene ninguna restriccion):
+dirr: str = "La ladera"
